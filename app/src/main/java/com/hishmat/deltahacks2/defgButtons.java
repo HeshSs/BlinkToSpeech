14d5250ad39b69bb518e721c13e160c0e183e853
HeshSs <hishmat.salehi2015@gmail.com>
2020-01-26 10:28:12 -0500

    Revert "Version 1"
    This reverts commit 6ddfa315

---
+++ app/src/main/java/com/hishmat/deltahacks2/defgButtons.java
@@ -0,0 +1,86 @@
+package com.hishmat.deltahacks2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class defgButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defg_buttons);
+
+        TextView inputText = findViewById(R.id.defgText);
+        inputText.setText(MainActivity.inputText.getText());
+
+        Button d = findViewById(R.id.buttonD);
+        d.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.inputText);
+                if (textView.getText().equals("input")) {
+                    textView.setText("D");
+                } else if (textView.getText().charAt(textView.getText().length()-1) == ' ') {
+                    textView.setText(textView.getText() + "D");
+                } else {
+                    textView.setText(textView.getText() + "d");
+                }
+            }
+        });
+
+        Button e = findViewById(R.id.buttonE);
+        e.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.inputText);
+                if (textView.getText().equals("input")) {
+                    textView.setText("E");
+                } else if (textView.getText().charAt(textView.getText().length()-1) == ' ') {
+                    textView.setText(textView.getText() + "E");
+                } else {
+                    textView.setText(textView.getText() + "e");
+                }
+            }
+        });
+
+        Button f = findViewById(R.id.buttonF);
+        f.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.inputText);
+                if (textView.getText().equals("input")) {
+                    textView.setText("F");
+                } else if (textView.getText().charAt(textView.getText().length()-1) == ' ') {
+                    textView.setText(textView.getText() + "F");
+                } else {
+                    textView.setText(textView.getText() + "f");
+                }
+            }
+        });
+
+        Button g = findViewById(R.id.buttonG);
+        g.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.inputText);
+                if (textView.getText().equals("input")) {
+                    textView.setText("G");
+                } else if (textView.getText().charAt(textView.getText().length()-1) == ' ') {
+                    textView.setText(textView.getText() + "G");
+                } else {
+                    textView.setText(textView.getText() + "g");
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/hishmat/deltahacks2/hijkActivity.java
+++ app/src/main/java/com/hishmat/deltahacks2/hijkActivity.java
@@ -2,46 +2,45 @@ package com.hishmat.deltahacks2;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class defgActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hijkActivity extends AppCompatActivity {
+    public static TextView textView = MainActivity.inputText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defg);
 
-        TextView inputText = findViewById(R.id.defgText);
+        TextView inputText = findViewById(R.id.typeText);
         inputText.setText(MainActivity.inputText.getText());
 
-        Button defg = findViewById(R.id.buttonOpenDEFG);
-        defg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDEFG();
-            }
-        });
-        Button hijk = findViewById(R.id.buttonGotoHIJK);
+        Button hijk = findViewById(R.id.buttonHIJK);
         hijk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gotoHIJK();
+                openHIJK();
+            }
+        });
+        Button lmno = findViewById(R.id.buttonLMNO);
+        lmno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoLMNO();
             }
         });
     }
-
-    public void openDEFG() {
-        Intent intent = new Intent(this, defgButtons.class);
+    public void openHIJK() {
+        Intent intent = new Intent(this, hijkButtons.class);
         startActivity(intent);
         finish();
     }
 
-    public void gotoHIJK() {
-        Intent intent = new Intent(this, hijkActivity.class);
+    public void gotoLMNO() {
+        Intent intent = new Intent(this, /*lmnoActivity*/hijkButtons.class);
         startActivity(intent);
         finish();
     }
